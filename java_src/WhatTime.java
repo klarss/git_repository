4a208570497404a1a3c06b6fa6702ab7d3f6fcc5
klarss <shadow96neko@gmail.com>
2019-06-10 22:56:50 +0900

    Create WhatTime.java
    オブジェクト指向の課題

---
+++ java_src/WhatTime.java
@@ -0,0 +1,31 @@
+/**
+ * WhatTime.java
+ * @author 17268026 shimizukoyu
+ */
+
+import java.util.Calendar;
+
+public class WhatTime implements Runnable{
+	private int hour;
+	private int min;
+	private int sec;
+	public void run() {
+		while(true) {
+			Calendar calendar = Calendar.getInstance();
+			hour = calendar.get(Calendar.HOUR_OF_DAY);
+			min = calendar.get(Calendar.MINUTE);
+			sec = calendar.get(Calendar.SECOND);
+			System.out.println(hour + ":" + min + ":" + sec);
+			try {
+				Thread.sleep(1000);
+			}
+			catch(InterruptedException e){
+				System.out.println(e);
+			}
+		}
+	}
+	public static void main(String[] args) {
+		WhatTime time = new WhatTime();
+		new Thread(time).start();
+	}
+}
